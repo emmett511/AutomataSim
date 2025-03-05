e3da3c08891764de945e0703fe713fef263382a8
Hunter Lindauer <holindauer@gmail.com>
2025-03-05 08:38:57 -0800

    fix: overridden test for run til complete

--- test_automata.py
+++ test_automata.py
@@ -2,7 +2,7 @@ import pytest
 from automata import Automata
 
 @pytest.fixture
-def testAutomata():
+def test_automata():
     states = {"q0", "q1", "q2", "q3"}
     alphabet = {'0', '1'}
 
@@ -20,68 +20,70 @@ def testAutomata():
 
     return Automata(states, alphabet, stateTransition, startingState, acceptStates)
 
+def test_set_input(test_automata):        
+    """tests setInput on cases where input words are valid and invalid"""
+    assert test_automata.setInput(['0','0','1','0','1']) == True         
+    assert test_automata.setInput(['1','0','2','0','1']) == False
 
-def test_setInput(testAutomata):        # tests setInput on cases where input words are valid and invalid
-    assert testAutomata.setInput(['0','0','1','0','1']) == True         
-    assert testAutomata.setInput(['1','0','2','0','1']) == False
+def test_valid_next_state(test_automata):       
+    """testing nextState when input is accepted"""
+    test_automata.setInput(['0','0','1','0','1'])
+    assert test_automata.getState() == "q0"
+    test_automata.nextState()
+    assert test_automata.getState() == "q1"
+    test_automata.nextState()
+    assert test_automata.getState() == "q1"
+    test_automata.nextState()
+    assert test_automata.getState() == "q2"
+    test_automata.nextState()
+    assert test_automata.getState() == "q3"
+    assert test_automata.nextState() == True
+    assert test_automata.getState() == "q2"
 
+def test_invalid_next_state(test_automata):        
+    """testing nextState when input is not accepted"""
+    test_automata.setInput(['0','1','0'])
+    assert test_automata.getState() == "q0"
+    test_automata.nextState()
+    assert test_automata.getState() == "q1"
+    test_automata.nextState()
+    assert test_automata.getState() == "q2"
+    assert test_automata.nextState() == False
+    assert test_automata.getState() == "q3"
 
-def test_validNextState(testAutomata):       # testing nextState when input is accepted
-    testAutomata.setInput(['0','0','1','0','1'])
-    assert testAutomata.getState() == "q0"
-    testAutomata.nextState()
-    assert testAutomata.getState() == "q1"
-    testAutomata.nextState()
-    assert testAutomata.getState() == "q1"
-    testAutomata.nextState()
-    assert testAutomata.getState() == "q2"
-    testAutomata.nextState()
-    assert testAutomata.getState() == "q3"
-    assert testAutomata.nextState() == True
-    assert testAutomata.getState() == "q2"
+def test_run_till_complete_accepted(test_automata):        
+    """testing run to completion when input is accepted"""
+    test_automata.setInput(['0','0','1','0','1'])
+    assert test_automata.runTillComplete() == True
 
-
-def test_invalidNextState(testAutomata):        # testing nextState when input is not accepted
-    testAutomata.setInput(['0','1','0'])
-    assert testAutomata.getState() == "q0"
-    testAutomata.nextState()
-    assert testAutomata.getState() == "q1"
-    testAutomata.nextState()
-    assert testAutomata.getState() == "q2"
-    assert testAutomata.nextState() == False
-    assert testAutomata.getState() == "q3"
-
-
-def test_runTillComplete(testAutomata):         # testing run to completion when input is accepted
-    testAutomata.setInput(['0','0','1','0','1'])
-    assert testAutomata.runTillComplete() == True
-
-
-def test_runTillComplete(testAutomata):         # testing run to completion when input is not accepted
-    testAutomata.setInput(['0','1','1'])
-    assert testAutomata.runTillComplete() == False
-
-
-def test_resetToBeginning(testAutomata):           # test resetToBeginning
-    testAutomata.setInput(['0','0','1','0','1']) 
-    testAutomata.runTillComplete()
-    testAutomata.resetToBeginning()
-    assert testAutomata.getState() == "q0"
+def test_run_till_complete_not_accepted(test_automata):        
+    """testing run to completion when input is not accepted"""
+    test_automata.setInput(['0','1','1'])
+    assert test_automata.runTillComplete() == False
 
 
-def test_prevState(testAutomata):            # test prevState
-    testAutomata.setInput(['0', '0', '1'])
-    testAutomata.runTillComplete()
-    assert testAutomata.getState() == "q2"
-    testAutomata.prevState()
-    assert testAutomata.getState() == "q1"
-    testAutomata.prevState()
-    assert testAutomata.getState() == "q1"
-    testAutomata.prevState()
-    assert testAutomata.getState() == "q0"
+def test_reset_to_beginning(test_automata):          
+    """testing resetToBeginning"""
+    test_automata.setInput(['0','0','1','0','1']) 
+    test_automata.runTillComplete()
+    test_automata.resetToBeginning()
+    assert test_automata.getState() == "q0"
 
 
-def test_emptyPrevState(testAutomata):           # test prevState when input is empty
-    testAutomata.prevState()
-    assert testAutomata.getState() == "q0"
+def test_prev_state(test_automata):           
+    """testing prevState"""
+    test_automata.setInput(['0', '0', '1'])
+    test_automata.runTillComplete()
+    assert test_automata.getState() == "q2"
+    test_automata.prevState()
+    assert test_automata.getState() == "q1"
+    test_automata.prevState()
+    assert test_automata.getState() == "q1"
+    test_automata.prevState()
+    assert test_automata.getState() == "q0"
+
+def test_empty_prev_state(test_automata):          
+    """testing prevState when input is empty"""
+    test_automata.prevState()
+    assert test_automata.getState() == "q0"
 
