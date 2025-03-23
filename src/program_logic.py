44eb39adab3dc77e4cf4382cb665be70d366e6b5
Hunter Lindauer <holindauer@gmail.com>
2025-03-22 17:31:09 -0700

    automata input compiles automtata or displays syntax err

--- src/program_logic.py
+++ src/program_logic.py
@@ -1,4 +1,14 @@
+from dsl import AutomatonDSL
+
 class ProgramLogic:
     def __init__(self):
-        pass
+        self.current_automata = None
 
+    def compile_automata(self, automaton_def: str):
+        """compiles an automata from description written in automaton dsl. 
+        Delegates compilation to the automaton dsl compiler."""
+        automaton = AutomatonDSL.parse_automaton(automaton_def)
+        if automaton:
+            self.current_automata = automaton
+        else:
+            self.current_automata = "SYNTAX ERROR:" + "\n\n" + automaton_def
--- src/simulation_page.py
+++ src/simulation_page.py
@@ -1,23 +1,22 @@
 import tkinter as tk
 from tkinter import simpledialog
-
+from program_logic import ProgramLogic
 
 class SimulationPage(tk.Frame):
     """Simulation page for inputting automata, strings, and running simulations."""
     
-    def __init__(self, parent):
+    def __init__(self, parent, program_logic: ProgramLogic):
         super().__init__(parent)
-
-        self.master.geometry("2000x1000")
+        self.program_logic = program_logic # NOTE: when combining all frames together, this will need to be rethought
         
+        # window size
+        self.master.geometry("2000x1000") # type: ignore
         # input automata button, opens input text box popup when clicked 
         self.input_button = tk.Button(self, text="Input Automata", command=self.get_automata_input)
         self.input_button.pack(pady=200)
-
         # Label to display the automata string
-        self.result_label = tk.Label(self, text="Automata: yet to be inputted...")
-        self.result_label.pack(pady=10)
-
+        self.automata_def_label = tk.Label(self, text="No automaton inputted yet...")
+        self.automata_def_label.pack(pady=10)
         # Store the automata string
         self.automata_description = "Automata yet to be inputted..."
 
@@ -30,21 +29,19 @@ class SimulationPage(tk.Frame):
         # text box
         text_widget = tk.Text(popup, width=60, height=20)
         text_widget.pack(padx=10, pady=10)
-        
-        def save_input():
-            user_input = text_widget.get("1.0", "end-1c")
-            if user_input:
-                self.automata_description = user_input
-                self.result_label.config(text=f"Automata: {user_input}")
+        # compile automata using control object
+        def compile_automata():
+            self.program_logic.compile_automata(text_widget.get("1.0", "end-1c"))
+            self.automata_def_label.config(text=self.program_logic.current_automata)
             popup.destroy()
         # button to accept user input            
-        save_button = tk.Button(popup, text="Accept", command=save_input)
+        save_button = tk.Button(popup, text="Accept", command=compile_automata)
         save_button.pack(pady=10)
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("Simulation Page")
-    page = SimulationPage(root)
+    page = SimulationPage(root, ProgramLogic())
     page.pack(fill="both", expand=True)
     root.mainloop()
